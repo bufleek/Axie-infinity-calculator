96514244572aa4c51d58e8c151521e5bc904496e
Bruno Kiprop <brunookiprop@gmail.com>
2021-10-03 18:23:50 +0300

    Free version libraries clean up

--- app/src/main/java/com/axieinfinity/energycalculator/ui/main/MainActivity.kt
+++ app/src/main/java/com/axieinfinity/energycalculator/ui/main/MainActivity.kt
@@ -5,16 +5,12 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android.billingclient.api.*
 import com.axieinfinity.energycalculator.R
 import com.axieinfinity.energycalculator.startFloatingService
 import com.axieinfinity.energycalculator.utils.AxieCalculator
@@ -22,12 +18,6 @@ import com.axieinfinity.energycalculator.utils.CardCounterActions
 import com.axieinfinity.energycalculator.utils.FloatingService.Companion.INTENT_COMMAND_FLOAT
 import com.axieinfinity.energycalculator.utils.FloatingWindowActions
 import com.axieinfinity.energycalculator.utils.drawOverOtherAppsEnabled
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.rewarded.RewardedAd
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -36,99 +26,14 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnWatchAds: Button
-    private lateinit var tvRewardsEarned: TextView
-    private lateinit var progressLoadingAd: ProgressBar
     private var themeColor: Int? = null
     private val dataStore by lazy { (application as AxieCalculator).dataStore }
-    private var rewardedAd: RewardedAd? = null
-    private var roundsExhaustedMessage: String? = null
-    private lateinit var rvSubs: RecyclerView
-    private val subsAdapter = SubsAdapter {
-        startBilling()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnWatchAds = findViewById(R.id.btn_watch_ads)
-        tvRewardsEarned = findViewById(R.id.tv_rewards)
-        progressLoadingAd = findViewById(R.id.progress_ad_loading)
-        rvSubs = findViewById(R.id.rv_subs)
-
-        rvSubs.apply {
-            adapter = subsAdapter
-            layoutManager = LinearLayoutManager(this@MainActivity)
-        }
-        MobileAds.initialize(this)
-
-        roundsExhaustedMessage = intent.getStringExtra(INTENT_ROUNDS_EXHAUSTED)
-        if (roundsExhaustedMessage != null) {
-            (application as AxieCalculator).rewardsEarned = 0
-            Snackbar.make(this, btnWatchAds, roundsExhaustedMessage!!, Snackbar.LENGTH_INDEFINITE)
-                .setAction("Ok") {}
-                .show()
-        }
-
-        val neededRewards = REWARDS_REQUIRED - (application as AxieCalculator).rewardsEarned
-        tvRewardsEarned.text = "${(application as AxieCalculator).rewardsEarned}/$REWARDS_REQUIRED"
-
-        btnWatchAds.setOnClickListener {
-            btnWatchAds.isEnabled = false
-            progressLoadingAd.visibility = View.VISIBLE
-            loadRewardedAd()
-        }
-        if (neededRewards <= 0) {
-            showFloatingWindow()
-        } else {
-            initAd()
-        }
-    }
-
-    private fun querySkuDetails() {
-        val skuList = ArrayList<String>().apply {
-            add("monthly_subscription")
-        }
-        val params = SkuDetailsParams.newBuilder()
-            .setSkusList(skuList)
-            .setType(BillingClient.SkuType.SUBS)
-            .build()
-        (application as AxieCalculator).billingClient.querySkuDetailsAsync(
-            params
-        ) { p0, p1 ->
-            if (p1 != null) {
-                billSku(p1[0])
-                showSubs(p1 as ArrayList<SkuDetails>)
-            }
-        }
-
-    }
-
-    private fun startBilling() {
-        subsAdapter.updateData(ArrayList(), true)
-        (application as AxieCalculator).billingClient.startConnection(object :
-            BillingClientStateListener {
-            override fun onBillingServiceDisconnected() {
-                Snackbar.make(
-                    this@MainActivity,
-                    rvSubs,
-                    "Failed to load subscriptions",
-                    Snackbar.LENGTH_INDEFINITE
-                )
-                    .setAction("Cancel") {
-                        showSubs(null)
-                    }
-                    .setAction("Retry") {
-                        startBilling()
-                    }.show()
-            }
-
-            override fun onBillingSetupFinished(p0: BillingResult) {
-                querySkuDetails()
-            }
-
-        })
+        showFloatingWindow()
     }
 
     private fun showFloatingWindow() {
@@ -138,16 +43,12 @@ class MainActivity : AppCompatActivity() {
                     requestPermission()
                 }
             } else {
-                if (roundsExhaustedMessage != null) {
-                    FloatingWindowActions.getInstance().listener?.onRoundsRefilled()
-                } else {
-                    startFloatingService(
-                        themeColor = themeColor ?: ContextCompat.getColor(
-                            this@MainActivity,
-                            R.color.overlay
-                        )
+                startFloatingService(
+                    themeColor = themeColor ?: ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.overlay
                     )
-                }
+                )
 
                 val colorPrefKey = intPreferencesKey("color")
                 val colorFlow: Flow<Int> = dataStore.data.map {
@@ -162,75 +63,6 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 finish()
-            }
-        }
-    }
-
-    private fun initAd() {
-        val adRequest = AdRequest.Builder().build()
-        RewardedAd.load(
-            this,
-            (application as AxieCalculator).adUnitId,
-            adRequest,
-            object : RewardedAdLoadCallback() {
-                override fun onAdFailedToLoad(p0: LoadAdError) {
-                    super.onAdFailedToLoad(p0)
-                }
-
-                override fun onAdLoaded(p0: RewardedAd) {
-                    super.onAdLoaded(p0)
-                    rewardedAd = p0
-                }
-            })
-    }
-
-    private fun loadRewardedAd() {
-        if (rewardedAd == null) {
-            val adRequest = AdRequest.Builder().build()
-            RewardedAd.load(
-                this,
-                (application as AxieCalculator).adUnitId,
-                adRequest,
-                object : RewardedAdLoadCallback() {
-                    override fun onAdFailedToLoad(p0: LoadAdError) {
-                        super.onAdFailedToLoad(p0)
-                        btnWatchAds.isEnabled = true
-                        progressLoadingAd.visibility = View.GONE
-                        Toast.makeText(this@MainActivity, "Failed to load Ad", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-
-                    override fun onAdLoaded(p0: RewardedAd) {
-                        super.onAdLoaded(p0)
-                        rewardedAd = p0
-                        btnWatchAds.isEnabled = true
-                        progressLoadingAd.visibility = View.GONE
-                        showRewardedAd()
-                    }
-                })
-        } else {
-            showRewardedAd()
-        }
-    }
-
-    private fun showRewardedAd() {
-        btnWatchAds.isEnabled = true
-        progressLoadingAd.visibility = View.GONE
-        rewardedAd?.let {
-            it.show(this) { rewardItem ->
-                rewardedAd = null
-                if ((application as AxieCalculator).rewardsEarned < REWARDS_REQUIRED) {
-                    (application as AxieCalculator).rewardsEarned++
-                }
-                if ((application as AxieCalculator).rewardsEarned < REWARDS_REQUIRED) {
-                    val rewards = (application as AxieCalculator).rewardsEarned
-                    tvRewardsEarned.text = "$rewards/$REWARDS_REQUIRED"
-                    Toast.makeText(this, "1 Reward Earned", Toast.LENGTH_SHORT)
-                        .show()
-                    initAd()
-                } else {
-                    showFloatingWindow()
-                }
             }
         }
     }
@@ -290,25 +122,6 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
             show()
-        }
-    }
-
-    private fun showSubs(skuDetails: List<SkuDetails>?) {
-        Toast.makeText(this, "called with ${skuDetails?.size ?: 0}", Toast.LENGTH_SHORT).show()
-        subsAdapter.updateData(ArrayList(skuDetails ?: emptyList()))
-    }
-
-    private fun billSku(sku: SkuDetails) {
-        val flowParams = BillingFlowParams.newBuilder()
-            .setSkuDetails(sku)
-            .build()
-        val responseCode = (application as AxieCalculator).billingClient.launchBillingFlow(
-            this@MainActivity,
-            flowParams
-        ).responseCode
-        if (responseCode != BillingClient.BillingResponseCode.OK) {
-            Toast.makeText(this, "Failed to launch billing process", Toast.LENGTH_LONG)
-                .show()
         }
     }
 
--- app/src/main/java/com/axieinfinity/energycalculator/utils/AxieCalculator.kt
+++ app/src/main/java/com/axieinfinity/energycalculator/utils/AxieCalculator.kt
@@ -4,22 +4,8 @@ import android.app.Application
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.PurchasesUpdatedListener
 
-class AxieCalculator: Application() {
-    val adUnitId = "ca-app-pub-3940256099942544/5224354917"
+class AxieCalculator : Application() {
     val dataStore: DataStore<Preferences> by preferencesDataStore("settings")
     var rewardsEarned = 0
-    private val purchasesUpdatedListener =
-        PurchasesUpdatedListener { billingResult, purchases ->
-            // To be implemented in a later section.
-        }
-
-    val billingClient by lazy {
-        BillingClient.newBuilder(this)
-            .setListener(purchasesUpdatedListener)
-            .enablePendingPurchases()
-            .build()
-    }
 }
